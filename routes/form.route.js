9fe3cc0c96ba4f158b112750a2cc5e7785c74d17
abhirampjayan <abhirampjayan@outlook.com>
2021-11-12 11:11:37 +0530

    done

--- routes/form.route.js
+++ routes/form.route.js
@@ -1,7 +1,32 @@
 import FormController from "../middleware/formController.js";
 import express from "express";
-const router = express.Router();
+import multer from "multer";
 
-router.route("/").get(FormController.formMulti);
+const router = express.Router();
+const upload = multer({ dest: "uploads/" });
+router.route("/").post(FormController.formMulti);
+router
+  .route("/upload")
+  .post(upload.single("document"), function (req, res, next) {
+    console.log(req.headers);
+    const header = req.headers;
+    if (!header.authorization) {
+      res.status(404).json({
+        error: {
+          statusCode: 404,
+          message: "authorization is required",
+        },
+      });
+    }else if(!req.file){
+      res.status(404).json({
+        error: {
+          statusCode: 404,
+          message: "document is required",
+        },
+      });
+    } else {
+      res.json({ data: "hello" });
+    }
+  });
 
 export default router;
